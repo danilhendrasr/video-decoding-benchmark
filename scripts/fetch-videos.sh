b9d2efa4ee6c888af18f172c1e46e52a7c0a1a6e
Danil <danilhendrasr@gmail.com>
2022-03-29 14:26:41 +0000

    chore: remove legacy scripts

--- scripts/fetch-videos.sh
+++
@@ -1,5 +0,0 @@
-if [ -f $(pwd)/videos/5-minutes.mkv ]; then
-  echo "Video sample already exists, skip fetching video"
-else
-  ffmpeg -t 300 -i https://cctvjss.jogjakota.go.id/rthp/rthp_segoro_amarto_tegalrejo_2.stream/playlist.m3u8 -c copy $(pwd)/videos/5-minutes.mkv
-fi
